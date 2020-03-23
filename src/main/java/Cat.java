7579fa34e11ab27e1fb53c27a11fd99e4ebf05b9
TondyNetsh <rt.netsh@live.com>
2020-03-23 12:32:12 +0200

    Second commit

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,7 +1,6 @@
 public class Cat extends Animal {
     @Override
     public void sound() {
-        super.sound();
         System.out.println("Cat meows");
     }
 }
--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,7 +1,6 @@
 public class Dog extends Animal {
     @Override
     public void sound() {
-        super.sound();
         System.out.println("Dog barks");
     }
 }
